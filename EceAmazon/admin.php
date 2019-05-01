b2b1d7994e9bd759ef2981dbc25fbe25011fb5dd
PaulFarault <paul.farault@gmail.com>
2019-05-01 19:12:53 +0200

    reset

---
+++ EceAmazon/admin.php
@@ -0,0 +1,12 @@
+<?php  $title = 'Admin'; ?>
+
+<?php ob_start(); ?>
+
+<div id="content">
+
+	<h1>Admin</h1>
+
+</div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
---
+++ EceAmazon/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Etablissement de la connexion
+$db = mysqli_connect("localhost", "root", "", "piscine");
+
+// Vérification de la connexion
+if (!$db) {
+    echo "Erreur : Impossible de se connecter à MySQL." . PHP_EOL;
+    echo "Errno de débogage : " . mysqli_connect_errno() . PHP_EOL;
+    echo "Erreur de débogage : " . mysqli_connect_error() . PHP_EOL;
+    exit();
+}
+
+// Initialisation des chemins
+define ('ROOT_PATH', realpath(dirname(__FILE__)));
+define('BASE_URL', 'http://localhost/complete-blog-php/');
+
+?>
