9753846ca90770c354e3902cbdf2fd076f6c3eab
PaulFarault <paul.farault@gmail.com>
2019-05-01 23:50:37 +0200

    ajout du favicon

--- template.php
+++ template.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+  <link rel="icon" href="img/favicon.ico" />
 	<title><?= $title ?></title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
