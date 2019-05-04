a025cc99f30ba89a485ff06c4321c9b84f347c4a
ThomasLrnt <laurent.thomas90@gmail.com>
2019-05-04 16:54:38 +0200

    Formulaire de paiement

--- livraison.php
+++ livraison.php
@@ -8,7 +8,7 @@
     
     
     <!-- Formulaire d'information sur la livraison -->
-    <form>
+    <form method="link" action="paiement.php">
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="inputNom">Nom :</label>
@@ -50,7 +50,9 @@
         </div>
             
             <div class="form-row">
-                <div class="form-group col-md-10"></div>
+                <div class="form-group col-md-10">
+                    <a href="panier.php"><button type="button" class="btn btn-primary">Retour au panier</button></a>
+                </div>
                 <div class="form-group col-md-2">
                 <button type="submit" class="btn btn-primary">Étape suivante</button>
                 </div>
--- paiement.php
+++ paiement.php
@@ -5,6 +5,49 @@
 <div id="content">
 
 	<h1>Information sur le paiement</h1>
+    
+    <!-- Formulaire sur le paiement -->
+    <form method="link" action="validation.php">
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <label for="inputNomCarte">Nom :</label>
+                <input type="text" class="form-control" id="inputNomCarte" placeholder="NomCarte">
+            </div>
+            <div class="form-group col-md-2">
+                <label for="inputType">Type :</label>
+                <select type="text" class="form-control" id="inputTypeCarte" placeholder="Type de carte">
+                    <option>Visa</option>
+                    <option>MasterCard</option>
+                    <option>American Express</option>
+                    <option>Paypal</option>
+                </select>
+            </div>
+            <div class="form-group col-md-2">
+                <label for="inputNomCarte">Date d'expiration :</label>
+                <input type="month" class="form-control" id="inputDateExp" placeholder="08-2019">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <label for="inputNumCarte">Numéro de la carte :</label>
+                <input type="number" class="form-control" id="inputNumCarte" placeholder="1234 2345 3456 4567">
+            </div>
+            <div class="form-group col-md-2">
+                <label for="inputSecuCarte">Code de sécurité :</label>
+                <input type="number" class="form-control" id="inputSecuCarte" placeholder="123">
+            </div>
+        </div><br><br>
+    
+        <div class="form-row">
+                <div class="form-group col-md-8">
+                    <a href="livraison.php"><button type="button" class="btn btn-primary">Retour à la livraison</button></a>
+                </div>
+                <div class="form-group col-md-2">
+                <button type="submit" class="btn btn-primary">Étape suivante</button>
+            </div>
+        </div>
+    </form>
+    
 
 </div>
 <?php $content = ob_get_clean(); ?>
