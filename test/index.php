04da964a4a2a46dabb751ff597cc73b105efceb0
PaulFarault <paul.farault@gmail.com>
2019-05-05 16:14:09 +0200

    tests session connexion

--- test/index.php
+++ test/index.php
@@ -1,17 +1,14 @@
-<?php require_once('config.php'); ?>
+<?php session_start();
 
-<?php  $title = 'Connexion'; ?>
+require_once '../config.php';
 
-<?php ob_start(); ?>
-
-	<?php
 	if (isset($_POST) AND !empty($_POST)) {
+
 		if (!empty(htmlspecialchars($_POST['mail'])) AND !empty(htmlspecialchars($_POST['mdp']))) {
 			$req = $db->prepare('SELECT * FROM utilisateur WHERE mail = :mail AND mdp = :mdp');
 			$req->execute([
 				'mail' => $_POST['mail'],
 				'mdp' => $_POST['mdp']
-
 			]);
 			$user = $req->fetchObject();
 			if ($user) {
@@ -30,28 +27,13 @@
 	}
 ?>
 
-    <form method="link" action="admin.php">
+    <form method="POST" action="index.php">
         
-        <div class="form-row">
-        	<form action="connexion.php" method="POST">
-                <div class="form-group col-md-6">
                 <label for="inputEmail">Adresse mail :</label>
                 <input type="mail" class="form-control" name ="mail" placeholder="adresse@exemple.com">
-                </div>  
-
-                <div class="form-group col-md-6">
                 <label for="inputPassword">Mot de passe :</label>
                 <input type="password" class="form-control" name="mdp" placeholder="Mot de passe">
-                </div>
 
                 <button class="btn btn-outline-secondary">Se connecter</button>
-            </form>
-        </div>
         
     </form>
-	
-
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
