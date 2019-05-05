11a2ac96dc9b5c411b83b282d4a7e298c85af216
PaulFarault <paul.farault@gmail.com>
2019-05-05 21:30:59 +0200

    implémentation page item

--- item.php
+++ item.php
@@ -14,15 +14,43 @@ if($resultat = mysqli_query($db,$sql)){
 	$item=mysqli_fetch_assoc($resultat);
 ?>
 
-<h2><?php echo(utf8_encode($item["nom"])); ?></h2>
+<h2><?php echo(utf8_encode($item["nom"])); ?> - <?php echo(utf8_encode($item["pu"])); ?> €</h2>
 
-<div class="row">
-	<div class="col-12">
-		<img src= <?php printf('"%s"', $item['photos']); ?> >
+<div class="row vignettes">
+	<div class="col-3">
+		<img class="img-fluid" src= <?php printf('"%s"', $item['photos']); ?> >
 	</div>
-	<div class="col-6"> <p><?php echo(utf8_encode($item["description"])); ?></p> </div>
-	<div class="col-6"></div>
 </div>
+<div class="row">
+	<div class="col">
+		<p><?php echo(utf8_encode($item["description"])); ?></p>
+	</div>
+	<div class="col-md-3">
+		
+		<form class="form-group row">
+			<label for="id" class="col-form-label">Quantité :</label>
+			<div class="col">
+  				<input class="form-control" id="qte" type="number" value="1" min="0" step="1" onblur="total()">
+  			</div>
+  		</form>
+  		<div class="row">
+  			<div class="col">
+  				<p><b>Total : <span id="tot"><?php echo(utf8_encode($item["pu"])); ?> €</b></span></p>
+  			</div>
+  		</div>
+  		<div class="row">
+  			<div class="col">
+  				<button type="button" class="btn btn-primary"><i class="fas fa-cart-plus"></i> | Ajouter au panier</button>
+  			</div>
+  		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	function total(){
+	document.getElementById("tot").innerHTML = <?=$item["pu"]; ?> * Number(document.getElementById("qte").value) + " €";}
+</script>
+
 
 <?php
 }
