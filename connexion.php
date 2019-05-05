6b4d66de7d63ba5f11c4987de3d6dcc256ffbe88
PaulFarault <paul.farault@gmail.com>
2019-05-05 14:37:24 +0200

    correction page connexion

--- connexion.php
+++ connexion.php
@@ -7,30 +7,15 @@
         
         <div class="form-row">
         
-            <div class="form-group col-md-6">
                 <div class="form-group col-md-6">
                 <label for="inputEmail">Adresse mail :</label>
                 <input type="mail" class="form-control" id="inputEmail" placeholder="adresse@exemple.com">
                 </div>  
-            </div>
-        <div/>
-            
-        <div class="form-row">
-            <div class="form-group col-md-6">
+
                 <div class="form-group col-md-6">
                 <label for="inputPassword">Mot de passe :</label>
                 <input type="mail" class="form-control" id="inputPassword" placeholder="Mot de passe">
                 </div>           
-            </div>
-        </div>
-            
-            <div class="form-group col-md-5">
-            </div>
-            <div class="form-group col-md-7">
-                
-                
-            </div>
-            
         </div>
         
     </form>
